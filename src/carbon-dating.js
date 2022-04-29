6fb8e25f25a36d663acf2bb0ce97d303fe1cf7ea
bluewater17 <96071740+bluewater17@users.noreply.github.com>
2022-04-29 21:50:10 +0300

    complete several tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,22 +6,34 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  const sampleActivityNum = Number(sampleActivity);
+  if (
+    sampleActivityNum &&
+    sampleActivityNum > 0 &&
+    sampleActivityNum < MODERN_ACTIVITY
+  ) {
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    const age = Math.log(MODERN_ACTIVITY / sampleActivityNum) / k;
+    return Math.ceil(age);
+  } else {
+    return false;
+  }
 }
+//console.log(dateSample(3));
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,33 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    const sorted = members
+      .filter((item) => typeof item === 'string')
+      .map((item) => item.replace(/^[ ]+/g, ''));
+    let result = [];
+    for (let i = 0; i < sorted.length; i++) {
+      result.push(sorted[i][0]);
+    }
+    const res = result.map((elem) => elem.toUpperCase());
+    return res.sort().join('');
+  }
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
